8ba15bcf18ddbf657065821caee6ead64ec86a15
LukeArmstrong-DuBois <123008264+LukeArmstrong-DuBois@users.noreply.github.com>
2025-01-26 11:37:46 -0700

    Uploading Homework 2

---
+++ HW2/script.js
@@ -0,0 +1,21 @@
+// three favorite websites
+console.log("My Favorite Websites:");
+console.log("1. Pokemon Showdown");
+console.log("2. YouTube");
+console.log("3. Spotify");
+
+// three favorite games
+console.log("My Favorite Games:");
+console.log("1. Xenoblade Chronicles");
+console.log("2. Minecraft");
+console.log("3. AI: The Somnium Files");
+
+// three favorite artists
+console.log("My Favorite Artists:");
+console.log("1. Salvador Dali - My aunt gave me a book of his work when I was a child, and it really inspired me. His work changed the surrealist movement in art and built a new understanding of art.");
+console.log("2. Kikuo - Their works have covered extremely serious topics while also being incredibly enjoyable and powerful to listen to. Their abilities in audio engineering are also unique, making each of their works sound unique and full.");
+console.log("3. Yoko Taro - This game developer has made some of my favorite game series like Nier and Drakengard. Each game is incredibly sad, but their stories are powerful and have changed a lot of gamers' lives, mine included.");
+
+// question for page viwer
+let favoriteColor = prompt("What's your favorite website/game/artist?");
+console.log("Your favorite is: " + Viwerfavorite);
