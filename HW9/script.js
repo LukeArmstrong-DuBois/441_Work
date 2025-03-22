e4c6f80afbf7885726cdb831ff175bd29829e5d7
LukeArmstrong-DuBois <123008264+LukeArmstrong-DuBois@users.noreply.github.com>
2025-03-22 17:32:44 -0600

    Delete HW9 directory

--- HW9/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("button").click(function () {
-        $.getJSON("data.json", function (data) {
-            $("#data-container").displayPokemon({
-                data: data.pokemon
-            });
-        });
-    });
-});
